80a8ace71f17b3bd088d21a6052b5856c9489135
Aidan <aidanenseleit98@gmail.com>
2020-06-13 20:42:24 +1000

    Login page works, still needs formatting

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,8 @@
 function createNewUser() {
+  const email = document.getElementById("emailSubmit").value;
+  const password = document.getElementById("passwordSubmit").value;
+  const result = document.getElementById("loginResult");
+
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
@@ -7,6 +11,7 @@ function createNewUser() {
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(errorCode, errorMessage);
+      result.innerHTML = "Password Must Contain a symbol, number and Uppercase Character";
       // ...
     });
 }
@@ -18,11 +23,50 @@ function userLogin() {
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
+    .then(function () {
+      window.location.href = "index.html";
+    })
     .catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(errorCode, errorMessage);
-      result.setAttribute("value", errorCode + errorMessage);
+      result.innerHTML = "Incorrect Password";
     });
+}
+function userLogout() {
+  const result = document.getElementById("loginResult");
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      result.innerHTML = "Log Out Successful.";
+    })
+    .catch(function (error) {
+      result.innerHTML = "Log Out Unsuccessful.";
+    });
+}
+
+function userStatus() {
+  const result = document.getElementById("loginResult");
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+
+      result.innerHTML = email + " is already logged in.";
+      // ...
+    } else {
+      // User is signed out.
+      result.innerHTML = "No user is logged in.";
+      // ...
+    }
+  });
 }
